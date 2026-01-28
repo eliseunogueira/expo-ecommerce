a23c8e678c65333f234d1d2743b6061d7f4b5b5e
Eliseu Nogueira <adm@eliseunogueira.com.br>
2026-01-28 11:03:40 -0300

    db config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
 import { ENV } from './config/env.js';
+import { clerkMiddleware } from '@clerk/express';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const __dirname = path.resolve();
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.get('/api/health', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Hello World!', auth: req.auth });
 });
 
 //make our app ready for deployment
@@ -15,4 +18,7 @@ if (ENV.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'));
   });
 }
-app.listen(ENV.PORT, () => console.log('Server running on port 3000 '));
+app.listen(ENV.PORT, () => {
+  console.log('Server running on port 3000 ');
+  connectDB();
+});
